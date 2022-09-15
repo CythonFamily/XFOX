c3d2a914f2b35171325c3443a70fd277e16a982d
CYTHON FAMILY <105876098+CythonFamily@users.noreply.github.com>
2022-09-15 08:54:32 +0100

    .

--- XBOX.py
+++ XBOX.py
@@ -10,6 +10,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("XBOX").checkin()
+		__import__("XBOX").Main_()
 	else:
-		__import__("XBOX").checkin()
+		__import__("XBOX").Main_()
